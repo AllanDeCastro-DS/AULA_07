4b820a41a4bbc8ee3d1c6aff7efc6213fc6dc172
Allan de Castro <116167880+AllanDeCastro-DS@users.noreply.github.com>
2024-09-27 12:07:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/*function acao() {
+  document.write("executando <br>");
+}
+//setInterval(acao, 2000);
+
+*/
+/*
+setInterval(() => {
+  document.write("coiso2<br>");
+  }, 3000);
+
+
+
+
+  var tempo = 0;
+  
+  setInterval(() => {
+    document.getElementById("tempor").innerHTML = " " + tempo;
+    tempo += 1;
+  }, 1000);
+
+
+*/
+// var timer = setInterval(() => {
+//   document.write("Executando3 <br>");
+// }, 1500);
+
+function acao2() {
+  document.write("executando4 <br>");
+}
+
+setTimeout(acao2(), 5000);
